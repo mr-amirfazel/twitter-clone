89a8e0faf8742d9637316257ff1cf124e28050cb
amirfazel <amirfazel45@gmail.com>
2023-04-02 02:05:25 +0430

    hide scrollbar

--- components/feed/index.tsx
+++ components/feed/index.tsx
@@ -25,7 +25,7 @@ const Feed:FC<FeedProps> = ({tweets: propTweets}) => {
     })
   }
   return (
-    <div className="border-x max-h-screen overflow-scroll">
+    <div className="border-x max-h-screen overflow-scroll scrollbar-hide">
         <div className="flex items-center justify-between">
             <h1 className='p-5 pb-0 text-xl font-bold'>Home</h1>
             <RefreshIcon onClick={handleRefresh} className='w-8 h-8 cursor-pointer text-twitter mr-5 mt-5 transition-all duration-500 ease-out hover:rotate-180 active:scale-125'/>
