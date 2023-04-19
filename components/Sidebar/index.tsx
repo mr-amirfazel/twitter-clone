6328a0dfbe9d5779795078e04092f7b450160293
amirfazel <amirfazel45@gmail.com>
2023-04-19 08:50:59 +0430

    few style changes

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -18,7 +18,7 @@ import SidebarRow from './SidebarRow';
 function Sidebar() {
   return (
     <div className="flex flex-col items-center px-4 md:items-start">
-      <img className='h-10 w-10 m-3' src="https://store-images.s-microsoft.com/image/apps.45406.9007199266244427.4d45042b-d7a5-4a83-be66-97779553b24d.2a88a418-b96d-44a6-ad4f-5e0ee6289b2c" alt="twitter-icon" />
+      <img className='h-10 w-10 m-3 rounded-full' src="https://store-images.s-microsoft.com/image/apps.45406.9007199266244427.4d45042b-d7a5-4a83-be66-97779553b24d.2a88a418-b96d-44a6-ad4f-5e0ee6289b2c" alt="twitter-icon" />
       <SidebarRow Icon={HomeIcon} title='Home' />
       <SidebarRow Icon={HashtagIcon} title='Explore' />
       <SidebarRow Icon={BellIcon} title='Notifications' />
